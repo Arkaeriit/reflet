b7e415f1eb6b67945999333de0113fd21e6cc94e
Arkaeriit <maxbouillot@gmail.com>
2019-12-12 15:49:48 +0100

    We can link correctly data and the program now start at the label start.

--- link64.c
+++ link64.c
@@ -1,16 +1,16 @@
-/*--------------------------------------------------------------------\
-||This file contain the function used to link various object files    |
-|into a single binary file. It's main task is to put the instruction  |
-|one after an other and handle lables used for function call or other |
-|branching. If the next line is a simple instruction it will be a     |
-|'i' followed by 64 bits of instruction. If it is the declaration     |
-|of a new label it will be a 'j' folowed by a 'd' followed by two     |
-|blank bytes followed by a the name of the label on 150 bytes. If     |
-|it is a branching or a function call it will be a 'j' followed       |
-|by a 'j' followed by two bytes used to know the opperand of the      |
-|reulting instruction, followed by the name of the label where to     |
-|jump.                                                                |
-\--------------------------------------------------------------------*/
+/*-------------------------------------------------------------------\
+||This file contain the function used to link various object files   |
+|into a single binary file. It's main task is to put the instruction |
+|one after an other and handle lables used for function call or other|
+|branching. If the next line is a simple instruction it will be a    |
+|'i' followed by 64 bits of instruction. If it is the declaration    |
+|of a new label it will be a 'j' folowed by a 'd' followed by two    |
+|blank bytes followed by a the name of the label on 150 bytes. If    |
+|it is a branching or a function call it will be a 'j' followed      |
+|by a 'j' followed by two bytes used to know the opperand of the     |
+|reulting instruction, followed by the name of the label where to    |
+|jump.                                                               |
+\-------------------------------------------------------------------*/
 
 #include "link64.h"
 
@@ -128,6 +128,24 @@ int l64_addLabel(labelHead* lB,const char* labelName, uint64_t position){
 }
 
 /*
+ * Indicate that we are defining a new data label
+ *  Arguments : 
+ *      dT : the chain with the labels
+ *      labelName : the name of the label
+ *      position : the position of the label in the linked code
+ *      data : the content of the data
+ *  return :
+ *      LINK_ERROR_DOUBLE_DEFINITION if a label with the same name is already placed
+ *      LABEL_OK otherwise
+ */
+int l64_addData(dataHead* dT,const char* labelName, char* content){
+    l64_addLabel(dT, labelName, dT->size + 1);
+    label_tag* tag = l64_autoGetLabel(dT, labelName);
+    tag->content = content;
+    return LABEL_OK;
+}
+
+/*
  * Indicate that we will call a label from a position
  *  Arguments :
  *      lB : the chain with the labels
@@ -141,16 +159,41 @@ void l64_addCall(labelHead* lB, const char* labelName, uint64_t position){
 }
 
 /*
+ * Indicate that we will load data from that position
+ *  Arguments :
+ *      dT : the chain with the data
+ *      labelName : the name of the label
+ *      position : the position from where the label is called
+ */
+void l64_loadData(dataHead* dT, const char* labelName, uint64_t position){
+    l64_addCall(dT, labelName, position); //Might change later
+}
+
+/*
+ * Add a first executed line of code into the list of instruction which will jump to a function named start
+ *  Argument :
+ *      cH : the list of instruction
+ *      lB : the list of label for jump
+ */
+void l64_firstInst(codeHead* cH, labelHead* lB){
+    char* opperand = malloc(sizeof(uint64_t));
+    *((uint64_t*) opperand) = JMP;
+    l64_addCall(lB, "start", cH->size);
+    l64_addList(cH, opperand, 'd');
+}
+
+/*
  * Add a file to the list representing all the code.
  *  Arguments : 
  *      cH : the list
  *      lB : the list with info concerning the labels
+ *      dT : the list with info concerning data
  *      fin : a pointer to the file we must add
  *  return :
  *      LINK_OK if everything went well
  *      an error code otherwise
  */
-int l64_addFile(codeHead* cH, labelHead* lB, FILE* fin){
+int l64_addFile(codeHead* cH, labelHead* lB, dataHead* dT, FILE* fin){
     OBJECT_MW_TYPE magicWord;
     fread(&magicWord, 1, OBJECT_MW_SIZE ,fin);
     if(magicWord != OBJECT_MW){
@@ -176,6 +219,27 @@ int l64_addFile(codeHead* cH, labelHead* lB, FILE* fin){
             }else{
                 return LINK_INVALID_LABEL;
             }
+        }else if(bufferStart == 'd'){
+            char* labelName = malloc(sizeof(char) * MAX_SIZE_LABEL);
+            char* opperand = malloc(sizeof(uint64_t)); //The start of the line of machine code
+            fread(&bufferStart, 1, 1, fin);
+            fread(opperand, 1, 2, fin);
+            fread(labelName, 1, MAX_SIZE_LABEL, fin);
+            char* data = malloc(sizeof(char) * SIZE_DATA);
+            if(bufferStart == 'd'){ //a new data label is defined 
+                fread(data, 1, SIZE_DATA, fin);
+                l64_addData(dT, labelName, data);
+            }else if(bufferStart == 'l'){
+                char reg;
+                fread(&reg, 1, 1, fin); //We read the register where we put the adress of the data
+                *((uint64_t*) opperand) |= (reg << ARG_SHIFT_1); //We put the register after the opperand
+                l64_loadData(dT, labelName, cH->size);
+                l64_addList(cH, opperand, 'd');
+                fread(data, 1, SIZE_DATA - 1, fin); //We ignore the rest of the line
+                free(data);
+            }else{
+                return LINK_INVALID_LABEL;
+            }
         }else{
             return LINK_INVALID_LABEL;
         }
@@ -187,12 +251,16 @@ int l64_addFile(codeHead* cH, labelHead* lB, FILE* fin){
  * Complete all the left alone lines about branching in an about-to-be-linked code.
  *  Arguments :
  *      cH : the list with the machine code
- *      lB : the list with the labels
+ *      lB : the list with the labels for jumps or data
+ *      shift : the shift between the opperand already in cH and 
+ *              the info in lB. This should be ARG_SHIFT_1 if lB is
+ *              typed as labelHead and ARG_SHIFT_2 if l is typed as
+ *              dataHead
  *  return :
  *      LINK_OK if everything went well
  *      an error code otherwise
  */
-int l64_branching(codeHead* cH, labelHead* lB){
+int l64_branching(codeHead* cH, labelHead* lB, uint64_t shift){
     for(uint64_t i=0; i<lB->size; i++){
         label_tag* tag = l64_getList(lB, i)->tag;
         if(!tag->alreadyPlaced){
@@ -201,10 +269,24 @@ int l64_branching(codeHead* cH, labelHead* lB){
         }
         for(uint64_t i=0; i<tag->numberOfCalls; i++){
             uint64_t* texte = (uint64_t*) l64_getList(cH, tag->places[i])->texte; //The line of code that need completion
-            *texte |= (tag->position << ARG_SHIFT_1);
+            *texte |= (tag->position << shift);
         }
     }
     return LINK_OK;
+}
+
+/*
+ * Write the content data to be loaded into the output file
+ *  Arguments:
+ *      dT : the lis with all the data
+ *      fout : the output file
+ */
+void l64_writingData(dataHead* dT, FILE* fout){
+    fwrite(&(dT->size), 1, sizeof(uint64_t), fout); //We write the total oumout af data to be loaded
+    for(uint64_t i=0; i<dT->size; i++){
+        label_tag* tag = l64_getList(dT, i)->tag;
+        fwrite(tag->content, 1, SIZE_DATA, fout);
+    }
 }
 
 /*
@@ -217,20 +299,28 @@ int l64_branching(codeHead* cH, labelHead* lB){
 int l64_link(FILE** fin,FILE* fout, int nFiles){
     codeHead* cH = l64_initList();
     labelHead* lB = l64_initList();
+    dataHead* dT = l64_initList();
+
+    l64_firstInst(cH, lB);
+
     for(int i=0; i<nFiles; i++){
-        if(l64_addFile(cH, lB, fin[i]) != LINK_OK){
+        if(l64_addFile(cH, lB, dT, fin[i]) != LINK_OK){
             fprintf(stderr, "Error : invalid object file(s).\n");
             return LINK_INVALID_FILE;
         }
     }
 
-    if(l64_branching(cH, lB) != LINK_OK){
+    if(l64_branching(cH, lB, ARG_SHIFT_1) != LINK_OK){
         return LINK_UNDEFINED_LABEL;
     }
+    if(l64_branching(cH, dT, ARG_SHIFT_2) != LINK_OK){
+        return LINK_UNDEFINED_LABEL;
+    }   
     
     BINARY_MW_TYPE magicWord = BINARY_MW;
     fwrite(&magicWord, 1, BINARY_MW_SIZE ,fout);
-    for(uint64_t i=0; i<cH->size;i++){
+    l64_writingData(dT, fout);
+    for(uint64_t i=0; i<cH->size;i++){ //We write the compiled code in the output file
         code* line = l64_getList(cH,i);
         fwrite(line->texte, 1, sizeof(uint64_t), fout);
     }
--- link64.h
+++ link64.h
@@ -8,13 +8,14 @@
 #include <inttypes.h>
 #include "constants.h"
 
-//this struct is used as the link in the chain representing label
+//this struct is used as the link in the chain representing label for jumps
 typedef struct label_tab_struct {
     char* name; 
     uint64_t* places;
-    uint64_t position;
+    uint64_t position; 
     uint64_t numberOfCalls;
     bool alreadyPlaced;
+    char* content; //used for adding data labels
 } label_tag;
 
 /*
@@ -33,19 +34,25 @@ typedef struct codeHead_struct {
     struct code_struct* next;
 } codeHead;
 
+typedef codeHead dataHead;
+typedef codeHead labelHead;
+
 codeHead* l64_initList();
 void l64_addList(codeHead* cH,char* texte, uint8_t label);
 code* l64_getList(codeHead* cH,uint64_t index);
 //
-typedef codeHead labelHead;
 bool l64_isLabelThere(labelHead* lB,const char* labelName, uint64_t* indx);
 label_tag* l64_autoGetLabel(labelHead* lB, const char* labelName);
 int l64_addLabel(labelHead* lB, const char* labelNamen, uint64_t position);
 void l64_addCall(labelHead* lB, const char* labelName, uint64_t position);
+int l64_addData(dataHead* dT, const char* labelNamen, char* content);
+void l64_loadData(dataHead* dT, const char* labelName, uint64_t position);
 //
 
-int l64_addFile(codeHead* cH, labelHead* lB, FILE* fin);
-int l64_branching(codeHead* cH, labelHead* lB);
+void l64_firstInst(codeHead* cH, labelHead* lB);
+int l64_addFile(codeHead* cH, labelHead* lB, dataHead* dT, FILE* fin);
+int l64_branching(codeHead* cH, labelHead* lB, uint64_t shift);
+void l64_writingData(dataHead* dT, FILE* fout);
 int l64_link(FILE** fin,FILE* fout,int nFiles);
 
 #endif
