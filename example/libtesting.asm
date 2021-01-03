976ad6fd95b1c4e653649b64861962155bfcd99d
Arkaeriit <maxbouillot@gmail.com>
2021-01-03 02:11:34 +0100

    Added nice comments in the testing file

--- example/libtesting.asm
+++ example/libtesting.asm
@@ -1,5 +1,7 @@
 wordsize 16
 ;This file contain tests for the various function of math.asm and printing.asm
+;It should be printed as 'reflet-nasm libtesting.asm math.asm printing.asm -o out.bin'
+;It can then be tested as following: (TODO when the simulator will have a nice cli write a tuto here)
 
 label testString
 data "Reflet is a neat ISA!"
@@ -17,33 +19,33 @@ label testingMath
     cpy R2
     callf intMult
     read R1
-    debug
+    debug ;expexting 13x5 = 195 = 0xc3
     set 15 ;testing intDiv
     cpy R1
     set 4
     cpy R2
     callf intDiv
     read R1
-    debug
+    debug ;expecting 15/4 = 3 = 0x3
     read R2
-    debug
+    debug ;expecting 15%4 = 3 = 0x3
     set 10 ;testing intPow
     cpy R1
     set 4
     cpy R2
     callf intPow
     read R1
-    debug
+    debug ;expecting 10^4 = 10000 = 0x2710
     ret
 
 label testingPrinting
     set+ 64
     cpy R1
-    callf printc
+    callf printc ;should print '@'
     setlab testString
     cpy R1
     callf strlen
     read R1
-    debug
+    debug ;expecting the length of the string which is 22 = 0x16
     ret
 
