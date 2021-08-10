959ae5d5e53822a11fdd07495ad9be07b8b6ee49
Arkaeriit <maxbouillot@gmail.com>
2021-08-10 11:09:26 +0200

    Used more restrictive 16 bit ram in 16 bit simulations.

--- rtl/simu/reflet_ram16.v
+++ rtl/simu/reflet_ram16.v
@@ -1,16 +1,11 @@
 /*-------------------------------------\
 |This module codes for a ram block with|
 |the same input and output addresses.  |
-|The words are 16 bits wide and each   |
-|byte can be addressed.                |
-|It is made by combining two 8-bits    |
-|modules in parallel, one for even     |
-|addresses and one for odd ones.       |
+|The words are 16 bits wide and accesse|   |
+|should be aligned to 16 bits.         |
 \-------------------------------------*/
 
 //Compute the size 
-`define size_even  (size/2 + size%2)
-`define size_odd   (size/2)
 
 module reflet_ram16 #(
     parameter addrSize = 9,
@@ -23,47 +18,38 @@ module reflet_ram16 #(
     input [addrSize-1:0] addr,
     input [15:0] data_in,
     input write_en,
-    output [15:0] data_out
+    output reg [15:0] data_out
     );
     
-    //Translate the addresses
-    wire [addrSize-1:0] addr_even = addr[addrSize-1:1] + addr[0];
-    wire [addrSize-1:0] addr_odd = addr[addrSize-1:1];
+    wire [addrSize-1:0] used_addr = addr[addrSize-1:1];
+    reg [15:0] ram[size-1:0];
 
-    //Combine the output of both block to generate the global data_out
-    wire [7:0] data_out_even;
-    wire [7:0] data_out_odd;
-    assign data_out = (addr[0] ? {data_out_even, data_out_odd} : {data_out_odd, data_out_even});
-
-    //Spliting tha data_in into two parts and give each one to a blocl
-    wire [7:0] data_in_even = (addr[0] ? data_in[15:8] : data_in[7:0]);
-    wire [7:0] data_in_odd = (!addr[0] ? data_in[15:8] : data_in[7:0]);
-
-    reflet_ram8 #(
-        .addrSize(addrSize),
-        .size(`size_even),
-        .resetable(resetable))
-    ram_even (
-        .clk(clk),
-        .reset(reset),
-        .enable(enable),
-        .addr(addr_even),
-        .data_in(data_in_even),
-        .write_en(write_en),
-        .data_out(data_out_even));
-
-    reflet_ram8 #(
-        .addrSize(addrSize),
-        .size(`size_odd),
-        .resetable(resetable))
-    ram_odd (
-        .clk(clk),
-        .reset(reset),
-        .enable(enable),
-        .addr(addr_odd),
-        .data_in(data_in_odd),
-        .write_en(write_en),
-        .data_out(data_out_odd));
+    generate
+        if(resetable)
+        begin
+            integer i;
+            always @ (posedge clk)
+                if(!reset)
+                begin
+                    for(i=0; i<size; i=i+1)
+                        ram[i] <= 0;
+                    data_out <= 0;
+                end
+                else
+                begin
+                    if(write_en)
+                        ram[used_addr] <= data_in;
+                    data_out <= ram[used_addr];
+                end
+        end
+        else
+            always @ (posedge cl)
+            begin
+                if(write_en)
+                    ram[used_addr] <= data_in;
+                    data_out <= ram[used_addr];
+            end
+        endgenerate
 
 endmodule
 
