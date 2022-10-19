6d598c82b56baa554e8b746cff40076f52659172
Maxime Bouillot <maxime.bouillot@withings.com>
2022-10-19 23:03:37 +0200

    Made a soft to test interrupts in the simulator.

---
+++ software/test_interrupt.asm
@@ -0,0 +1,29 @@
+; A small program that tests the interrupts in the simulator.
+; It exits after 3 interruptions which all generate a debug sign
+
+; R09 contains the counter of interrupts
+
+label ISR
+    push
+    set 1
+    add R09
+    debug
+    cpy R09
+    pop
+    retint
+
+label start
+    set 0
+    cpy R9
+    setlab ISR
+    setint 1
+    set8 0x10
+    cpy SR
+    label loop
+        set 2
+        les R09
+        cmpnot
+        jifl loop
+    quit
+
+
