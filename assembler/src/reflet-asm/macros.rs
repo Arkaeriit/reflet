c2b1172dc5473dd65e1b5a88f0bbf09c47aac215
Maxime Bouillot <maxbouillot@gmail.com>
2023-07-01 17:55:38 +0200

    Reuse format_string_into_number for set8

--- assembler/src/reflet-asm/macros.rs
+++ assembler/src/reflet-asm/macros.rs
@@ -1,3 +1,4 @@
+use macro_asm_builder::utils::format_string_into_number;
 
 /// Runs all the implementation-specifics macros
 pub fn macros(code: &Vec<String>) -> Result<Option<String>, String> {
@@ -12,8 +13,12 @@ fn set8(code: &Vec<String>) -> Result<Option<String>, String> {
     match code[0].as_str() {
         "@set8" => {
             if code.len() == 2 {
-                match format_string_into_byte(&code[1]) {
-                    Some(num) => Ok(Some(format_set8(num))),
+                match format_string_into_number(&code[1]) {
+                    Some((num, _)) => if num <= 0xFF {
+                        Ok(Some(format_set8(num.try_into().unwrap())))
+                    } else {
+                        Err(format!("Error, macro @set8 should take a single 1-byte number as argument."))
+                    },
                     None => Err(format!("Error, macro @set8 should take a single 1-byte number as argument.")),
                 }
             } else {
@@ -33,44 +38,5 @@ fn format_set8(num: u8) -> String {
     cpy R12
     set {}
     or R12", (num & 0xF0) >> 4, num & 0x0F)
-}
-
-/// Decode a byte. Return none if it is invalid
-fn format_string_into_byte(s: &str) -> Option<u8> {
-    if s.len() == 0 {
-        None
-    } else {
-        if &s[0..1] == "-" {
-            match i8::from_str_radix(s, 10) {
-                Ok(num) => {
-                    let positive = num * -1;
-                    let true_positive = positive as u8;
-                    let not = !true_positive;
-                    let cc2 = not + 1;
-                    Some(cc2)
-                },
-                Err(_) => None,
-            }
-        } else {
-            if s.len() > 3 {
-                if &s[0..2] == "0x" {
-                    match u8::from_str_radix(&s[2..s.len()], 0x10) {
-                        Ok(num) => Some(num),
-                        Err(_) => None,
-                    }
-                } else {
-                    match u8::from_str_radix(s, 10) {
-                        Ok(num) => Some(num),
-                        Err(_) => None,
-                    }
-                }
-            } else {
-                match u8::from_str_radix(s, 10) {
-                    Ok(num) => Some(num),
-                    Err(_) => None,
-                }
-            }
-        }
-    }
 }
 
